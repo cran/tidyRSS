d0fe14ea2e0a1a9c8910a4e2411c283502525b62
Robert Myles McDonnell <robertmylesmcdonnell@gmail.com>
2017-06-07 16:22:31 +0000

    version 1.2.0

--- R/tidyfeed.R
+++ R/tidyfeed.R
@@ -1,13 +1,12 @@
-#' @import dplyr
 #' @importFrom magrittr "%>%"
-#' @importFrom RCurl getURL
-#' @import utils
-#' @import httr
-#' @import XML
-#' @importFrom purrr map
-#' @importFrom xml2 read_xml
-#' @importFrom xml2 as_list
+#' @importFrom tibble tibble
+#' @importFrom httr GET
 #' @importFrom lubridate parse_date_time
+#' @importFrom xml2 read_xml
+#' @importFrom xml2 xml_text
+#' @importFrom xml2 xml_find_all
+#' @importFrom xml2 xml_find_first
+#' @importFrom dplyr select
 #' @author Robert Myles McDonnell, \email{robertmylesmcdonnell@gmail.com}
 #' @references \url{https://en.wikipedia.org/wiki/RSS}
 #' @title Extract a tidy data frame from RSS and Atom feeds
@@ -15,294 +14,177 @@
 #' produces a tidy data frame, easy to use for further manipulation and
 #' analysis.
 #' @param feed (\code{character}). The url for the feed that you want to parse.
-#' @return A tidy data frame that contains the following elements, assuming
-#' they exist in the feed itself:
-#'
-#'  item_title: The title of each feed post.
-#'
-#'  item_date: The date of publciation. Returns \code{NA} if this
-#' does not exist.
-#'
-#'  item_link: The original url of the item.
-#'
-#'  summary: a brief summary of the item, if it exists..
-#'
-#'  subtitle: the subtitle of the item, if it exists.
-#'
-#'  creator: The author of the item, if this exists in the feed.
-#'
-#'  categories: The categories used for indexing the item, separated
-#'  by a semi-colon, if this exists in the feed.
-#'
-#'  head_title: title of the url from the header of the feed
-#'
-#'  head_link: url for header
-#'
-#'  last_updated: date (POSIXct format) last updated.
-#'
+#' @param result (\code{character}), one of:
+#'   - "all": both the feed information (title, url, etc.) and the item information (title, creator, etc.) are returned.
+#'   - "feed": Only the information from the feed is returned, not the individual feed items.
+#'   - "items": opposite of the above.
 #' @examples
-#' # RSS feed:
-#'
-#' tech <- tidyfeed("http://feeds.feedburner.com/techcrunch")
-#'
+
 #' # Atom feed:
 #' r_j <- tidyfeed("http://journal.r-project.org/rss.atom")
 #'
-#' # raw xml feed:
-#' cisc <- tidyfeed("http://tools.cisco.com/security/center/eventResponses_20.xml")
-#' ## (not a feed)
-#' ny <- tidyfeed("http://www.nytimes.com/index.html?partner=rssnyt")
-#'
-#'
 #' @export
-tidyfeed <- function(feed){
 
-  # first, clean the feed
+tidyfeed <- function(feed, result = c("all", "feed", "items")){
+  invisible({
+  suppressWarnings({
   if(!grepl("http://", feed)){
     feed <- strsplit(feed, "://")[[1]][2]
     feed <-paste0("http://", feed)
   }
 
-  # error message
   msg <- "\nThis page does not appear to be a suitable feed.\nHave you checked that you entered the url correctly?\nIf you are certain that this is a valid rss feed, please file an issue at: https://github.com/RobertMyles/tidyRSS/issues"
 
-  # tidy the feeds
-  tidy_all <- function(document){
+  formats <- c("a d b Y H:M:S z", "a, d b Y H:M z",
+               "Y-m-d H:M:S z", "d b Y H:M:S",
+               "d b Y H:M:S z", "a b d H:M:S z Y",
+               "a b dH:M:S Y")
 
-    # date formats, taken from R package feedeR by A. Collier;
-    # https://github.com/DataWookie/feedeR/blob/master/R/read.R;
-    # added one extra for an error that came up in testing and adjusted
-    # the first.
-    formats <- c("a d b Y H:M:S z", "a, d b Y H:M z",
-                 "Y-m-d H:M:S z", "d b Y H:M:S",
-                 "d b Y H:M:S z", "a b d H:M:S z Y",
-                 "a b dH:M:S Y")
+  choice <- match.arg(result, choices = c("all", "feed", "items"))
 
-    if(class(document) != "list"){
-      return(message(msg))
-    }
+  doc <- httr::GET(feed) %>% xml2::read_xml()
 
 
-    rss_tidy <- function(document){
+  if(grepl("http://www.w3.org/2005/Atom", xml2::xml_attr(doc, "xmlns"))){
 
-      document <- document[["channel"]]
-      head_doc <- document[names(document) != "item"]
-      item_doc <- document[names(document) == "item"]
+    ns <- xml2::xml_ns_rename(xml2::xml_ns(doc), d1 = "atom")
 
-      if(length(item_doc) == 0){
+    entries <- xml2::xml_find_all(doc, "atom:entry[position()>1]", ns = ns)
 
-        items <- head_doc$items$Seq
-        at <- list()
-        for(i in 1:length(items)){
-          at[[i]] <- attributes(items[i]$li)$resource
-        }
-        at <- unlist(at)
-        df <- dplyr::data_frame(item_url = at,
-                                head_title = unlist(head_doc$title),
-                                head_link = unlist(head_doc$link),
-                                last_updated = unlist(head_doc$date))
-
-        df$last_updated <- parse_date_time(df$last_updated,
-                                           orders = formats)
-      } else{
-
-        df <- dplyr::data_frame(item_title = unlist(map(item_doc,
-                                                        "title")))
-
-        # date
-        if("pubDate" %in% names(item_doc[[1]])){
-          df$item_date <- unlist(map(item_doc, "pubDate"))
-          df$item_date <- parse_date_time(df$item_date, orders = formats)
-        } else if("pubdate" %in% names(item_doc[[1]])){
-          df$item_date <- unlist(map(item_doc, "pubdate"))
-          df$item_date <- parse_date_time(df$item_date, orders = formats)
-        } else {
-          df$item_date <- NA
-        }
-
-        # links
-        if("origLink" %in% names(item_doc$item)){
-          df$item_link <- unlist(map(item_doc, "origLink"))
-        } else if(!is.null(item_doc$item$link)){
-          df$item_link <- unlist(map(item_doc, "link"))
-        } else{
-          df$item_link <- "None provided"
-        }
-
-        # subtitle
-        if("subtitle" %in% names(item_doc$item)){
-          df$subtitle <- unlist(map(item_doc, "subtitle"))
-        }
-
-        # summary
-        if("summary" %in% names(item_doc$item)){
-          df$summary <- unlist(map(item_doc, "summary"))
-        }
-
-        # creator:
-        if("creator" %in% names(item_doc$item)){
-          cre <- vector("list", length(item_doc))
-          for(i in 1:length(item_doc)){
-            if("creator" %in% names(item_doc[[i]])){
-              cre[[i]] <- item_doc[[i]][grep("creator",
-                                             names(item_doc[[i]]))]
-              attr(cre[[i]], "names") <- NULL
-              cre <- map(cre, unlist)
-              cre[[i]] <- paste(cre[[i]], sep="", collapse="; ")
-              cre[i] <- unlist(cre[i])
-              for(i in 1:length(cre)){
-                if(is.null(cre[[i]])){
-                  cre[[i]] <- NA
-                }
-                }
-              }
-          }
-          df$creator <- unlist(cre)
-        } else if("author" %in% names(item_doc$item)){
-            df$creator <- unlist(map(item_doc, "author"))
-            }
-
-        # get categories:
-        if("category" %in% names(item_doc$item)){
-          cat_check <- unlist(map(item_doc, "category"))
-          if(!is.null(cat_check)){
-            cats <- vector("list", length(item_doc))
-            for(i in 1:length(item_doc)){
-              if("category" %in% names(item_doc[[i]])){
-                cats[[i]] <- item_doc[[i]][grep("category",
-                                                names(item_doc[[i]]))]
-                attr(cats[[i]], "names") <- NULL
-                cats <- map(cats, unlist)
-                cats[[i]] <- paste(cats[[i]], sep="", collapse="; ")
-                cats[i] <- unlist(cats[i])
-              }
-            }
-            for(i in 1:length(cats)){
-              if(is.null(cats[[i]])){
-                cats[[i]] <- NA
-              }
-            }
-            df$item_categories <- unlist(cats)
-            }
-          }
-
-        # head title
-        df$head_title <- unlist(head_doc$title)
-
-        # head link
-        if(length(grep("link", names(head_doc))) > 1){
-          x_link <- head_doc[grep("link", names(head_doc))]
-          x_link <- unlist(x_link[grep("http", x_link)])
-          attr(x_link, "names") <- NULL
-          df$head_link <- x_link[1]
-        } else{
-          df$head_link <- unlist(head_doc$link)
-        }
-
-        # dates
-        if("lastBuildDate" %in% names(head_doc)){
-          df$last_updated <- unlist(head_doc$lastBuildDate)
-          df$last_updated <- parse_date_time(df$last_updated,
-                                             orders = formats)
-        }
-      }
-      return(df)
-    }
-
-    atom_tidy <- function(document){
-
-      entries <- document[grep("entry", names(document))]
-
-      if(length(entries) == 0){
-        return(message(msg))
-      }
-
-      df <- dplyr::data_frame(item_title = unlist(map(entries, "title")),
-                              item_date = unlist(map(entries, "updated")),
-                              item_link = unlist(map(entries, "id")),
-                              head_title = unlist(document$title))
-
-      if(length(grep("link", names(document))) > 1){
-
-        x_link <- document[grep("link", names(document))]
-        x_link <- attr(x_link[[2]], "href")
-        df$head_link <- x_link
-
-      } else{
-        df$head_link <- unlist(document$link)
-      }
-
-      df$last_update <- parse_date_time(unlist(document$updated),
-                                        orders = formats)
-
-      # dates
-      xx <- try(unlist(map(entries, "updated")), silent = F)
-      if(class(xx) == "try-error"){
-        df$item_date <- NA
-      } else {
-        df$item_date <- parse_date_time(xx, orders = formats)
-      }
-
-      return(df)
-    }
-
-
-    names(document) <- tolower(names(document))
-
-
-    if(names(document)[1] == "channel"){
-      document <- rss_tidy(document)
-    } else{
-      document <- atom_tidy(document)
-    }
-    return(document)
-  }
-
-
-  options(show.error.messages= FALSE)
-
-
-  # get url requests
-
-  suppressWarnings(
-  document1 <- try(
-      getURL(feed) %>%
-      read_xml() %>%
-      as_list(),
-      silent = F)
-  )
-
-  suppressWarnings(
-  document2 <- try(
-    httr::GET(feed) %>%
-    XML::htmlParse() %>%
-    XML::xmlToList() %>%
-      .[[1]] %>%
-      .[[1]],
-    silent = F)
-  )
-
-  if(class(document1) != "try-error"){
-    rss <- tidy_all(document1)
-  } else if(class(document2) != "try-error"){
-    rss <- tidy_all(document2)
-  } else{
-    suppressWarnings(
-    document <- try(
-      httr::GET(feed) %>%
-        read_xml() %>%
-        as_list(),
-      silent = F)
+    res <- tibble::tibble(
+      feed_title = xml2::xml_text(xml2::xml_find_all(doc, ns = ns, "atom:title")),
+      feed_link = xml2::xml_text(xml2::xml_find_first(doc, ns = ns, "atom:id")),
+      feed_author = xml2::xml_text(xml2::xml_find_first(doc, ns = ns, "atom:author")),
+      feed_last_updated = xml2::xml_text(xml2::xml_find_first(doc, ns = ns, "atom:updated")),
+      item_title = xml2::xml_text(xml2::xml_find_first(entries, ns = ns, "atom:title")),
+      item_date_updated = xml2::xml_text(xml2::xml_find_first(entries, ns = ns,
+                                                            "atom:updated")) %>%
+        lubridate::parse_date_time(orders = formats),
+      item_link = xml2::xml_text(xml2::xml_find_first(entries, ns = ns,
+                                                    "atom:id")),
+      item_content = xml2::xml_text(xml2::xml_find_first(entries, ns = ns, "atom:content"))
     )
 
-    if(class(document) == "try-error"){
+    if(!grepl("http", res$feed_link)){
+      res$feed_link <- xml2::xml_text(xml2::xml_find_first(entries, ns = ns, "atom:origLink"))
+      if(!grepl("http", res$feed_link)){
+        res$feed_link <- xml2::xml_text(xml2::xml_find_first(entries, ns = ns, "atom:link"))
+      }
+    }
 
-      return(message(msg))
+    return(res)
+
+  } else{
+
+    channel <- xml2::xml_find_all(doc, "channel")
+
+    if(length(channel) == 0){
+      ns <- xml2::xml_ns_rename(xml2::xml_ns(doc), d1 = "rss")
+      channel <- xml2::xml_find_all(doc, "rss:channel", ns = ns)
+      site <- xml2::xml_find_all(doc, "rss:item", ns = ns)
+
+      res <- tibble::tibble(
+        feed_title = xml2::xml_text(xml2::xml_find_all(channel, "rss:title", ns = ns)),
+        feed_link = xml2::xml_text(xml2::xml_find_all(channel, "rss:link", ns = ns)),
+        feed_description = xml2::xml_text(xml2::xml_find_first(channel, "rss:description", ns = ns)),
+        feed_last_updated = xml2::xml_text(xml2::xml_find_first(channel,
+                                                                "rss:lastBuildDate", ns = ns)) %>%
+          lubridate::parse_date_time(orders = formats),
+        feed_language = xml2::xml_text(xml2::xml_find_first(channel, "rss:language", ns = ns)),
+        feed_update_period = xml2::xml_text(xml2::xml_find_first(channel, "rss:updatePeriod", ns = ns)),
+        item_title = xml2::xml_text(xml2::xml_find_all(site, "rss:title", ns = ns)),
+        item_creator = xml2::xml_text(xml2::xml_find_first(site, "rss:creator", ns = ns)),
+        item_date_published = xml2::xml_text(xml2::xml_find_first(site, "rss:pubDate", ns = ns)) %>%
+          lubridate::parse_date_time(orders = formats),
+        item_date =  xml2::xml_text(xml2::xml_find_first(site, "rss:date", ns = ns)) %>%
+         lubridate::parse_date_time(orders = formats),
+        item_subject = xml2::xml_text(xml2::xml_find_first(site, ns = ns, "rss:subject")),
+        item_category1 = xml2::xml_text(xml2::xml_find_first(site, "rss:category[1]", ns = ns)),
+        item_category2 = xml2::xml_text(xml2::xml_find_first(site, "rss:category[2]", ns = ns)),
+        item_category3 = xml2::xml_text(xml2::xml_find_first(site, "rss:category[3]", ns = ns)),
+        item_category4 = xml2::xml_text(xml2::xml_find_first(site, "rss:category[4]", ns = ns)),
+        item_category5 = xml2::xml_text(xml2::xml_find_first(site, "rss:category[5]", ns = ns)),
+        item_link = xml2::xml_text(xml2::xml_find_all(site, "rss:link", ns = ns)),
+        item_description = xml2::xml_text(xml2::xml_find_first(site, "rss:description", ns = ns))
+      )
+
+      res <- Filter(function(x) !all(is.na(x)), res)
+
+     if(choice == "feed"){
+       res <- res %>%
+        dplyr::select(feed_title, feed_link, feed_description, feed_last_updated)
+
+      return(res)
+
+    } else if(choice == "items"){
+      res <- res %>%
+        dplyr::select(item_title, item_creator, item_date_published,
+                      item_category1, item_category2, item_category3,
+                      item_category4, item_category5, item_link)
+
+      return(res)
 
     } else{
-      rss <- tidy_all(document)
+      return(res)
+      }
+    } else{
+
+      site <- xml2::xml_find_all(channel, "item")
+
+
+      if(choice == "all"){
+
+        res <- tibble::tibble(
+          feed_title = xml2::xml_text(xml2::xml_find_first(channel, "id")),
+          feed_link = xml2::xml_text(xml2::xml_find_first(channel, "link")),
+          feed_description = xml2::xml_text(xml2::xml_find_first(channel, "description")),
+          feed_last_updated = xml2::xml_text(xml2::xml_find_first(channel,
+                                                                  "lastBuildDate")) %>%
+            lubridate::parse_date_time(orders = formats),
+          feed_language = xml2::xml_text(xml2::xml_find_first(channel, "language")),
+          feed_update_period = xml2::xml_text(xml2::xml_find_first(channel, "updatePeriod")),
+          item_title = xml2::xml_text(xml2::xml_find_first(site, "title")),
+          item_creator = xml2::xml_text(xml2::xml_find_first(site, "dc:creator")),
+          item_date_published = xml2::xml_text(xml2::xml_find_first(site, "pubDate")) %>%
+            lubridate::parse_date_time(orders = formats),
+          item_category1 = xml2::xml_text(xml2::xml_find_first(site, "category[1]")),
+          item_category2 = xml2::xml_text(xml2::xml_find_first(site, "category[2]")),
+          item_category3 = xml2::xml_text(xml2::xml_find_first(site, "category[3]")),
+          item_category4 = xml2::xml_text(xml2::xml_find_first(site, "category[4]")),
+          item_category5 = xml2::xml_text(xml2::xml_find_first(site, "category[5]")),
+          item_link = xml2::xml_text(xml2::xml_find_first(site, "link"))
+        )
+
+        suppressWarnings(
+          res$feed_update_period[is.na(res$feed_update_period)] <- xml2::xml_text(
+            xml2::xml_find_first(channel, "sy:updatePeriod"))
+        )
+        suppressWarnings(
+        res$feed_title[is.na(res$feed_title)] <- xml2::xml_text(
+          xml2::xml_find_first(channel, "title"))
+        )
+
+        res <- Filter(function(x) !all(is.na(x)), res)
+
+        return(res)
+
+      } else if(choice == "feed"){
+        res <- res %>%
+          dplyr::select(feed_title, feed_link, feed_description, feed_last_updated)
+
+        return(res)
+
+      } else{
+        res <- res %>%
+          dplyr::select(item_title, item_creator, item_date_published,
+                        item_category1, item_category2, item_category3,
+                        item_category4, item_category5, item_link)
+
+        return(res)
+
+      }
     }
-    options(show.error.messages = TRUE)
   }
-  return(rss)
+  })
+  })
 }
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-[![Travis-CI Build Status](https://travis-ci.org/RobertMyles/tidyRSS.svg?branch=master)](https://travis-ci.org/RobertMyles/tidyRSS) [![CRAN\_Status\_Badge](https://www.r-pkg.org/badges/version/tidyRSS)](https://cran.r-project.org/package=tidyRSS)
+[![Travis-CI Build Status](https://travis-ci.org/RobertMyles/tidyRSS.svg?branch=master)](https://travis-ci.org/RobertMyles/tidyRSS) [![CRAN\_Status\_Badge](https://www.r-pkg.org/badges/version/tidyRSS)](https://cran.r-project.org/package=tidyRSS) [![CRAN\_Download\_Badge](http://cranlogs.r-pkg.org/badges/tidyRSS)](https://CRAN.R-project.org/package=tidyRSS) [![CRAN\_Download\_Badge](http://cranlogs.r-pkg.org/badges/grand-total/tidyRSS)](https://CRAN.R-project.org/package=tidyRSS)
 
 tidyRSS is a package for extracting data from [RSS feeds](https://en.wikipedia.org/wiki/RSS).
 
-It is easy to use as it only has one function, `tidyfeed()`, which takes one argument, the url of the feed. Running this function will return a tidy data frame of the information contained in the feed. If the url is not a feed, it will return an error message.
+It is easy to use as it only has one function, `tidyfeed()`, which takes one argument, the url of the feed. Running this function will return a tidy data frame of the information contained in the feed. If the url is not an rss or atom feed, it will return an error message.
 
-Included in the package is a simple dataset, a list of feed urls, which you can use to experiment with (they were taken from [here](https://raw.githubusercontent.com/DataWookie/feedeR/master/tests/testthat/test-feeds.txt)). One (<http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml>) returns NULL for most fields, although it is a valid field (and so throws an actual error). So bug reports (and suggestions for fixing them) are very welcome.
+Included in the package is a simple dataset, a list of feed urls, which you can use to experiment with. You can access this with `data("feeds")`.
 
 Installation
 ------------
@@ -36,22 +36,52 @@ library(tidyRSS)
 data("feeds")
 
 # select a feed:
-url <- feeds$feeds[[1]]
+rss <- sample(feeds$feeds, 1)
 
-tidyfeed(url)
-#> 
-#> This page does not appear to be a suitable feed.
-#> Have you checked that you entered the url correctly?
+tidyfeed(rss)
+#> # A tibble: 20 x 15
+#>      feed_title               feed_link
+#>           <chr>                   <chr>
+#>  1 Ars Technica https://arstechnica.com
+#>  2 Ars Technica https://arstechnica.com
+#>  3 Ars Technica https://arstechnica.com
+#>  4 Ars Technica https://arstechnica.com
+#>  5 Ars Technica https://arstechnica.com
+#>  6 Ars Technica https://arstechnica.com
+#>  7 Ars Technica https://arstechnica.com
+#>  8 Ars Technica https://arstechnica.com
+#>  9 Ars Technica https://arstechnica.com
+#> 10 Ars Technica https://arstechnica.com
+#> 11 Ars Technica https://arstechnica.com
+#> 12 Ars Technica https://arstechnica.com
+#> 13 Ars Technica https://arstechnica.com
+#> 14 Ars Technica https://arstechnica.com
+#> 15 Ars Technica https://arstechnica.com
+#> 16 Ars Technica https://arstechnica.com
+#> 17 Ars Technica https://arstechnica.com
+#> 18 Ars Technica https://arstechnica.com
+#> 19 Ars Technica https://arstechnica.com
+#> 20 Ars Technica https://arstechnica.com
+#> # ... with 13 more variables: feed_description <chr>,
+#> #   feed_last_updated <dttm>, feed_language <chr>,
+#> #   feed_update_period <chr>, item_title <chr>, item_creator <chr>,
+#> #   item_date_published <dttm>, item_category1 <chr>,
+#> #   item_category2 <chr>, item_category3 <chr>, item_category4 <chr>,
+#> #   item_category5 <chr>, item_link <chr>
 ```
 
-More information is contained in the vignette: `vignette("tidyrss", package = "tidyRSS")`. For common feed types, tidyRSS should be fast. For some feeds, there can be a slight delay because `tidyfeed()` is testing different ways of parsing the feed.
+More information is contained in the vignette: `vignette("tidyrss", package = "tidyRSS")`.
 
 Issues
 ------
 
-RSS feeds can be finicky things, if you find one that doesn't work with `tidyfeed()`, [let me know](https://github.com/robertmyles/tidyrss/issues). Please include the url of the feed that you are trying. Pull requests and general feedback are welcome.
+RSS & atom XML feeds can be finicky things, if you find one that doesn't work with `tidyfeed()`, [let me know](https://github.com/robertmyles/tidyrss/issues). Please include the url of the feed that you are trying. Pull requests and general feedback are welcome. Many feeds are malformed. What this means is that, for a well-formed feed, you'll get back a tidy data frame with information on the feed and the individual items (like blog posts, for example), including content. For malformed feeds, it will be less than this, as `tidyfeed()` deletes `NA` columns, where the information wasn't in the feed in the first place.
 
 Related
 -------
 
-The package is a 'tidy' version of two other related fantastic little packages, [rss](https://github.com/noahhl/r-does-rss) and [feedeR](https://github.com/DataWookie/feedeR), both of which return lists. In comparison to feedeR, tidyRSS returns more information from the RSS feed (if it exists), and develpment on rss seems to have stopped some time ago. Both packages were influences for tidyRSS.
+The package is a 'tidy' version of two other related fantastic little packages, [rss](https://github.com/noahhl/r-does-rss) and [feedeR](https://github.com/DataWookie/feedeR), both of which return lists. In comparison to feedeR, tidyRSS returns more information from the RSS feed (if it exists), and development on rss seems to have stopped some time ago. Both packages were influences for tidyRSS.
+
+### Version 1.2.0
+
+I've completely re-written the main function in this package, streamlining it by reducing the number of dependencies. The use of lists has been left behind for dealing with xml directly, which I think makes the package quicker and much more robust (not to mention much lighter in terms of code). For this little leap, I have the many question-answerers on Stack Overflow and the estimable [Guilherme Jardim Duarte](https://github.com/duarteguilherme) to thank, not to mention [hrbrmaster](https://github.com/hrbrmstr), who first alerted me to the bloatedness of this little package.
